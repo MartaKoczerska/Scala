15e37823b57fad980a5085461c3e5bb589dfff42
MartaKoczerska <44616522+MartaKoczerska@users.noreply.github.com>
2020-06-10 16:13:04 +0200

    Update Main.scala

--- Main.scala
+++ Main.scala
@@ -1,10 +1,12 @@
 object Main extends App {
   trait GeneralTree[A] {
-  case class Leaf[A] (val data:A) extends GeneralTree[A]
+  case class Leaf (val data:A) extends GeneralTree[A]
   case class Node (val data:A, val children:Seq[Node]) extends GeneralTree[A]
   }
   def size(n:Node):Int = {
-  n.children.foldLeft(0)((s,c) => s + size(c))}
+  n.children.foldLeft(0)((s,c) => s + size(c))
+  }
+  
   def height(n:Node):Int = {
   1+n.children.foldLeft(-1)((h,c) => h max height(c))
   }
