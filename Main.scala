121b14769ec5a7b5683fc8c2597306db135e1bc8
MartaKoczerska <44616522+MartaKoczerska@users.noreply.github.com>
2020-06-11 11:00:29 +0200

    Update Main.scala

--- Main.scala
+++ Main.scala
@@ -4,10 +4,10 @@ object Main extends App {
   case class Node (val data:A, val children:Seq[Node]) extends GeneralTree[A]
   }
   def size(n:Node):Int = {
-  n.children.foldLeft(0)((s,c) => s + size(c))
+  n.children.foldLeft(0)((s,c) => s + size(c)) -- trzeba zamienic na tail recursion
   }
   
   def height(n:Node):Int = {
-  1+n.children.foldLeft(-1)((h,c) => h max height(c))
+  1+n.children.foldLeft(-1)((h,c) => h max height(c)) -- trzeba zamienic na tail recursion
   }
   }
